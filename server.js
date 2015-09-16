b580c0b59d3c9ed9d698e965cb081eab9692daa8
Lai 'Lain' Jiang <lain.lai.jiang@gmail.com>
2015-09-16 16:25:31 -0500

    Modified port to use env var; added procfile

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var express = require('express');
 var partials = require('express-partials');
 var util = require('./lib/utility');
 
- var handler = require('./lib/request-handler');
+var handler = require('./lib/request-handler');
 
 var app = express();
 
